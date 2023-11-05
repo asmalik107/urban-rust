8f1646bdc71cfdaa79c3fb8851cfbe1a0282eda8
asmalik107 <asmalik@outlook.com>
2023-11-05 11:10:11 +0000

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use actix_web::{web, App, HttpServer};
+use std::{env};
+use actix_web::{middleware, web, App, HttpServer};
 
 mod base;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
     HttpServer::new(|| {
         App::new()
+            .wrap(middleware::Logger::default())
             .service(base::core)
             .service(base::echo)
             .route("/hey", web::get().to(base::manual_hello))
